1b60ae82b4e2303c799de164a1a01f8313e6733a
s1f10220187 <107394091+1f10220187@users.noreply.github.com>
2024-09-17 02:39:46 +0900

    デザイン完成

--- daily/forms.py
+++ daily/forms.py
@@ -5,8 +5,26 @@ class DailyForm(forms.ModelForm):
     class Meta:
         model = Daily
         fields = ['title', 'content']
+    
+    def __init__(self, *args, **kwargs):
+        super(DailyForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
 
 class CommentForm(forms.ModelForm):
     class Meta:
        model = Comment
        fields = ['text']
+      
+    def __init__(self, *args, **kwargs):
+        super(CommentForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['text'].widget.attrs.update({
+        'rows': '2',  # 縦の行数を減らす（例: 3行）
+        'style': 'height: 70px;'  # 高さを具体的に指定する（例: 100px）
+    })
